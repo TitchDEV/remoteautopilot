d2b23dd9a4b7fa1140c3cfcfe953ae8e0538349f
Sherwood Williamson <sherwoodw28@outlook.com>
2024-02-21 09:36:09 +0000

    Clean Up and Added Support
    Added support for normal numbers and enter key for functionality, better notification communication cleaned up the code.

--- ShipRemoteAutopilot/ShipRemoteAutopilot.cs
+++ ShipRemoteAutopilot/ShipRemoteAutopilot.cs
@@ -16,11 +16,12 @@ namespace ShipRemoteAutopilot
         bool CounterActive = false;
         bool isTimerDone = false;
         bool isShipAligned = false;
+        bool keysMode;
 
         AstroObject planetaryBody;
 
 
-        //10 Seconds countdown for moving ship to air
+        //Enumerator Coroutines for the Upwards thrust and Alignment functions.
         IEnumerator ThrusterTiming()
         {
             WaitForSeconds wait = new WaitForSeconds(10);
@@ -28,7 +29,6 @@ namespace ShipRemoteAutopilot
 
             isTimerDone = true;
         }
-        //2 Second countdown for aligning the ship to the surface of the planet for landing
         IEnumerator ShipAlignmentTiming()
         {
             WaitForSeconds wait = new WaitForSeconds(2);
@@ -38,34 +38,50 @@ namespace ShipRemoteAutopilot
         }
 
 
-        //Links the Align and Land Ship functions to the Autopilot Arrival flag
+        //Links the AlignShip() Function to be ran when OnArriveAtDestination() occurs.
         private void Start()
         {
+
             LoadManager.OnCompleteSceneLoad += (scene, loadScene) =>
             {
                 if (loadScene != OWScene.SolarSystem) return;
+
                 var shipBody = FindObjectOfType<ShipBody>();
                 shipBody.GetComponent<Autopilot>().OnArriveAtDestination += AlignShip;
+
+                keysMode = ModHelper.Config.GetSettingsValue<bool>("Alternate Keys Mode");
             };
         }
 
         //Function to Align the ship to face the floor of the planet
         public void AlignShip(float arrivalError)
         {
+            var notificationData = new NotificationData(NotificationTarget.Player, "Autopilot - Ship arrived at destination, entering orbit...", 5f, true);
+            NotificationManager.SharedInstance.PostNotification(notificationData, false);
+
+            //Re-enable the ships collision to correctly land on surface.
             Locator.GetShipBody().EnableCollisionDetection();
+
+            //Get the current reference frame of currently selected planet.
             var AligningReferenceFrame = planetaryBody.GetOWRigidbody().GetReferenceFrame();
+
+            //Align the ship with the planets surface.
             Locator.GetShipBody().GetComponent<AlignShipWithReferenceFrame>().OnEnterLandingMode(AligningReferenceFrame);
+            
+            //No idea WHAT this does but- its here.
             StartCoroutine(ShipAlignmentTiming());
         }
         
-        //Function to activate autopilot to location
+        //Actual function for activating the traditional Outer Wild's Autopilot.
         public void TravelToLocation()
         {
-            //Sets the timer to be done
+            //Sets the thruster timer to be complete again.
             isTimerDone = false;
 
+            //Gets the reference frame for the selected planet to give to Autopilot component.
             var ShipReferenceFrame = planetaryBody.GetOWRigidbody().GetReferenceFrame();
-            ModHelper.Console.WriteLine("Ship Reference Frame Name : " + ShipReferenceFrame);
+            
+            //Targets the reference frame and activates the Autopilot module.
             Locator.GetPlayerTransform().GetComponent<ReferenceFrameTracker>().TargetReferenceFrame(ShipReferenceFrame);
             Locator.GetShipBody().GetComponent<Autopilot>().FlyToDestination(ShipReferenceFrame);
         }
@@ -73,96 +89,167 @@ namespace ShipRemoteAutopilot
 
         private void Update()
         {
-            //Controls upward thrust when counter is counting
+            //This is performed when "Enter" is pressed, Enter sets this to true and starts firing the Thrusters
             if (CounterActive)
             {
                 Locator.GetShipBody().GetComponent<ThrusterModel>().AddTranslationalInput(Vector3.up);
             }
 
-            //When the ship is aligned 
+            //Catch to stop the ship going mental when the ship is aligned correctly.
             if (isShipAligned)
             {
                 Locator.GetShipBody().GetComponent<AlignShipWithReferenceFrame>().OnExitLandingMode();
                 isShipAligned = false;
             }
 
-            //When the timer is done, activate the travelling from orbit
+            //When the 10 second Thruster timer is complete, it sets this to True and, as show, disables the upwards thruster and itself, and begins travel to the destination.
             if (isTimerDone)
             {
                 CounterActive = false;
                 isTimerDone = false;
+                
+                var notificationData = new NotificationData(NotificationTarget.Player, "Thrusters Complete. Beginning travel to destination...", 5f, true);
+                NotificationManager.SharedInstance.PostNotification(notificationData, false);
+
                 TravelToLocation();
             }
 
-            //Go to Location
-            if (Keyboard.current.numpadEnterKey.wasPressedThisFrame)
-            {
-                Locator.GetShipBody().DisableCollisionDetection();
-                Locator.GetShipBody().GetComponent<LandingPadManager>()._isLanded = true;
 
-                if (Locator.GetShipBody().GetComponent<LandingPadManager>()._isLanded == true)
+            if (keysMode == false)
+            {
+                //Activation key for the Remote Autopilot function.
+                if (Keyboard.current.numpadEnterKey.wasPressedThisFrame)
                 {
+
+                    var notificationData = new NotificationData(NotificationTarget.Player, "Autopilot Engaged - Firing Upward Thrusters...", 5f, true);
+                    NotificationManager.SharedInstance.PostNotification(notificationData, false);
+
+                    //Disables the ships collision to stop it colliding with the sun and- yknow- dying.
+                    Locator.GetShipBody().DisableCollisionDetection();
+
+                    //Starts the Upwards thrust and begins the timer so that it disables when neccesary.
                     CounterActive = true;
                     StartCoroutine(ThrusterTiming());
                 }
-            }
 
 
-            //Select the Planet from the Planet Index
-            if (Keyboard.current.numpad1Key.wasPressedThisFrame)
-            {
-                planetaryBody = Locator.GetAstroObject(AstroObject.Name.TimberHearth);
-                var notificationData = new NotificationData(NotificationTarget.All, "AP - Destination Set - Timber Hearth", 10f, true);
-                NotificationManager.SharedInstance.PostNotification(notificationData, false);
-            }
+                //Options to choose the location in which the ship should fly to. The player is notified on their Suits HUD which item is selected.
+                if (Keyboard.current.numpad1Key.wasPressedThisFrame)
+                {
+                    planetaryBody = Locator.GetAstroObject(AstroObject.Name.TimberHearth);
+                    var notificationData = new NotificationData(NotificationTarget.Player, "AP - Destination Set - Timber Hearth", 5f, true);
+                    NotificationManager.SharedInstance.PostNotification(notificationData, false);
+                }
 
-            if (Keyboard.current.numpad2Key.wasPressedThisFrame)
-            {
-                planetaryBody = Locator.GetAstroObject(AstroObject.Name.GiantsDeep);
-                var notificationData = new NotificationData(NotificationTarget.All, "AP - Destination Set - Giant's Deep", 10f, true);
-                NotificationManager.SharedInstance.PostNotification(notificationData, false);
-            }
+                if (Keyboard.current.numpad2Key.wasPressedThisFrame)
+                {
+                    planetaryBody = Locator.GetAstroObject(AstroObject.Name.GiantsDeep);
+                    var notificationData = new NotificationData(NotificationTarget.All, "AP - Destination Set - Giant's Deep", 5f, true);
+                    NotificationManager.SharedInstance.PostNotification(notificationData, false);
+                }
 
-            if (Keyboard.current.numpad3Key.wasPressedThisFrame)
-            {
-                planetaryBody = Locator.GetAstroObject(AstroObject.Name.BrittleHollow);
-                var notificationData = new NotificationData(NotificationTarget.All, "AP - Destination Set - Brittle Hollow", 10f, true);
-                NotificationManager.SharedInstance.PostNotification(notificationData, false);
-            }
+                if (Keyboard.current.numpad3Key.wasPressedThisFrame)
+                {
+                    planetaryBody = Locator.GetAstroObject(AstroObject.Name.BrittleHollow);
+                    var notificationData = new NotificationData(NotificationTarget.All, "AP - Destination Set - Brittle Hollow", 5f, true);
+                    NotificationManager.SharedInstance.PostNotification(notificationData, false);
+                }
 
-            if (Keyboard.current.numpad4Key.wasPressedThisFrame)
-            {
-                planetaryBody = Locator.GetAstroObject(AstroObject.Name.DarkBramble);
-                var notificationData = new NotificationData(NotificationTarget.All, "AP - Destination Set - Dark Bramble", 10f, true);
-                NotificationManager.SharedInstance.PostNotification(notificationData, false);
-            }
+                if (Keyboard.current.numpad4Key.wasPressedThisFrame)
+                {
+                    planetaryBody = Locator.GetAstroObject(AstroObject.Name.DarkBramble);
+                    var notificationData = new NotificationData(NotificationTarget.All, "AP - Destination Set - Dark Bramble", 5f, true);
+                    NotificationManager.SharedInstance.PostNotification(notificationData, false);
+                }
 
-            if (Keyboard.current.numpad5Key.wasPressedThisFrame)
-            {
-                planetaryBody = Locator.GetAstroObject(AstroObject.Name.TowerTwin);
-                var notificationData = new NotificationData(NotificationTarget.All, "AP - Destination Set - Ash Twin", 10f, true);
-                NotificationManager.SharedInstance.PostNotification(notificationData, false);
-            }
+                if (Keyboard.current.numpad5Key.wasPressedThisFrame)
+                {
+                    planetaryBody = Locator.GetAstroObject(AstroObject.Name.TowerTwin);
+                    var notificationData = new NotificationData(NotificationTarget.All, "AP - Destination Set - Ash Twin", 5f, true);
+                    NotificationManager.SharedInstance.PostNotification(notificationData, false);
+                }
 
-            if (Keyboard.current.numpad6Key.wasPressedThisFrame)
-            {
-                planetaryBody = Locator.GetAstroObject(AstroObject.Name.CaveTwin);
-                var notificationData = new NotificationData(NotificationTarget.All, "AP - Destination Set - Ember Twin", 10f, true);
-                NotificationManager.SharedInstance.PostNotification(notificationData, false);
+                if (Keyboard.current.numpad6Key.wasPressedThisFrame)
+                {
+                    planetaryBody = Locator.GetAstroObject(AstroObject.Name.CaveTwin);
+                    var notificationData = new NotificationData(NotificationTarget.All, "AP - Destination Set - Ember Twin", 5f, true);
+                    NotificationManager.SharedInstance.PostNotification(notificationData, false);
+                }
+
+                if (Keyboard.current.numpad7Key.wasPressedThisFrame)
+                {
+                    planetaryBody = Locator.GetAstroObject(AstroObject.Name.Comet);
+                    var notificationData = new NotificationData(NotificationTarget.All, "AP - Destination Set - The Interloper", 5f, true);
+                    NotificationManager.SharedInstance.PostNotification(notificationData, false);
+                }
             }
             
-            if (Keyboard.current.numpad7Key.wasPressedThisFrame)
+            else if (keysMode == true)
             {
-                planetaryBody = Locator.GetAstroObject(AstroObject.Name.Comet);
-                var notificationData = new NotificationData(NotificationTarget.All, "AP - Destination Set - The Interloper", 10f, true);
-                NotificationManager.SharedInstance.PostNotification(notificationData, false);
-            }
+                //Activation key for the Remote Autopilot function.
+                if (Keyboard.current.enterKey.wasPressedThisFrame)
+                {
+                   var notificationData = new NotificationData(NotificationTarget.Player, "Autopilot Engaged - Firing Upward Thrusters...", 5f, true);
+                    NotificationManager.SharedInstance.PostNotification(notificationData, false);
 
-            if (Keyboard.current.numpad8Key.wasPressedThisFrame)
-            {
-                planetaryBody = Locator.GetAstroObject(AstroObject.Name.RingWorld);
-                var notificationData = new NotificationData(NotificationTarget.All, "AP - Destination Set - <REDACTED>", 10f, true);
-                NotificationManager.SharedInstance.PostNotification(notificationData, false);
+                    //Disables the ships collision to stop it colliding with the sun and- yknow- dying.
+                    Locator.GetShipBody().DisableCollisionDetection();
+
+                    //Starts the Upwards thrust and begins the timer so that it disables when neccesary.
+                    CounterActive = true;
+                    StartCoroutine(ThrusterTiming());
+                }
+
+
+                //Options to choose the location in which the ship should fly to. The player is notified on their Suits HUD which item is selected.
+                if (Keyboard.current.digit1Key.wasPressedThisFrame)
+                {
+                    planetaryBody = Locator.GetAstroObject(AstroObject.Name.TimberHearth);
+                    var notificationData = new NotificationData(NotificationTarget.Player, "AP - Destination Set - Timber Hearth", 5f, true);
+                    NotificationManager.SharedInstance.PostNotification(notificationData, false);
+                }
+
+                if (Keyboard.current.digit2Key.wasPressedThisFrame)
+                {
+                    planetaryBody = Locator.GetAstroObject(AstroObject.Name.GiantsDeep);
+                    var notificationData = new NotificationData(NotificationTarget.All, "AP - Destination Set - Giant's Deep", 5f, true);
+                    NotificationManager.SharedInstance.PostNotification(notificationData, false);
+                }
+
+                if (Keyboard.current.digit3Key.wasPressedThisFrame)
+                {
+                    planetaryBody = Locator.GetAstroObject(AstroObject.Name.BrittleHollow);
+                    var notificationData = new NotificationData(NotificationTarget.All, "AP - Destination Set - Brittle Hollow", 5f, true);
+                    NotificationManager.SharedInstance.PostNotification(notificationData, false);
+                }
+
+                if (Keyboard.current.digit4Key.wasPressedThisFrame)
+                {
+                    planetaryBody = Locator.GetAstroObject(AstroObject.Name.DarkBramble);
+                    var notificationData = new NotificationData(NotificationTarget.All, "AP - Destination Set - Dark Bramble", 5f, true);
+                    NotificationManager.SharedInstance.PostNotification(notificationData, false);
+                }
+
+                if (Keyboard.current.digit5Key.wasPressedThisFrame)
+                {
+                    planetaryBody = Locator.GetAstroObject(AstroObject.Name.TowerTwin);
+                    var notificationData = new NotificationData(NotificationTarget.All, "AP - Destination Set - Ash Twin", 5f, true);
+                    NotificationManager.SharedInstance.PostNotification(notificationData, false);
+                }
+
+                if (Keyboard.current.digit6Key.wasPressedThisFrame)
+                {
+                    planetaryBody = Locator.GetAstroObject(AstroObject.Name.CaveTwin);
+                    var notificationData = new NotificationData(NotificationTarget.All, "AP - Destination Set - Ember Twin", 5f, true);
+                    NotificationManager.SharedInstance.PostNotification(notificationData, false);
+                }
+
+                if (Keyboard.current.digit7Key.wasPressedThisFrame)
+                {
+                    planetaryBody = Locator.GetAstroObject(AstroObject.Name.Comet);
+                    var notificationData = new NotificationData(NotificationTarget.All, "AP - Destination Set - The Interloper", 5f, true);
+                    NotificationManager.SharedInstance.PostNotification(notificationData, false);
+                }
             }
         }
     }
